8e6d15dae4c0a20bda394a2f66acf9d757cb1af7
Ana Nikolova <ani.nik19@gmail.com>
2018-08-26 09:50:01 +0200

    done

--- starter_code_cypress/app.js
+++ starter_code_cypress/app.js
@@ -1,5 +1,25 @@
+const SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const hbs = require('hbs');
+
+
+// Set up
+const clientId = 'b79b10bd0d1d44d3a5acc36879f307e2',
+    clientSecret = 'bc73737ef95b4813a145eece06135f12';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
+
 
 const app = express();
 
@@ -11,6 +31,45 @@ app.use(express.static('public'));
 app.get('/', (req,res,next) => {
   res.render('home')
 })
+
+
+app.get('/artists', (req, res, next) => {
+  spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      res.render("artists", {artists:data.body.artists.items})
+    })
+    .catch(err => {
+     console.log("Something went wrong", err)
+    })
+});
+
+app.get('/albums/:artistId', (req, res, next) => {
+  spotifyApi.getArtistAlbums(req.params.artistId)
+    .then(data => {
+      console.log(data.body.items)
+      res.render('albums', {
+        albums: data.body.items
+      });
+    })
+    .catch(err => {
+      console.log('Something went wrong!', err);
+    });
+});
+
+app.get('/tracks/:albumId', (req, res, next) => {
+  spotifyApi.getAlbumTracks(req.params.albumId)
+    .then(data => {
+      console.log(data.body.items)
+      res.render('tracks', {
+        tracks: data.body.items
+      });
+    })
+    .catch(err => {
+      console.log('Something went wrong!', err);
+    });
+});
+
+
 
 
 app.listen(3000, () => {
